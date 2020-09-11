4796718f095825ba80916c396e5e826dcc171c1e
zl454 <531720074@qq.com>
2020-09-11 11:53:17 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="kylin-tabs">
   <div class="kylin-tabs-nav" ref="container">
-    <div class="kylin-tabs-nav-item" :class="{selected:t===selected}" v-for="(t,index) in titles" :key="index" :ref="el=>{if (el) navItems[index] =el}" @click="select(t)">{{t}}</div>
+    <div class="kylin-tabs-nav-item" :class="{selected:t===selected}" v-for="(t,index) in titles" :key="index" :ref="el=>{if (t===selected) selectedItem =el}" @click="select(t)">{{t}}</div>
     <div class="kylin-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="kylin-tabs-content">
@@ -24,24 +24,20 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([]);
+    const selectedItem = ref < HTMLDivElement[] > ([]);
     const indicator = ref < HTMLDivElement[] > (null);
     const container = ref < HTMLDivElement[] > (null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
       const {
         width
-      } = result.getBoundingClientRect();
+      } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const {
         left: left1
       } = container.value.getBoundingClientRect();
       const {
         left: left2
-      } = result.getBoundingClientRect();
+      } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -60,12 +56,12 @@ export default {
       }
     });
     return {
-      navItems,
       titles,
       defaults,
       select,
       indicator,
       container,
+      selectedItem,
     };
   },
 };
