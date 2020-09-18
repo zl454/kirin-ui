c2819a659262222db0173c47f5e326d5228f8a00
zl454 <531720074@qq.com>
2020-09-18 22:22:32 +0800

    简介

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -1,5 +1,19 @@
 <template>
 <div>
-  简介
+  <h1>Kylin-UI</h1>
+  <p>
+    这是我的第一个具有代表性的 Vue 造轮子项目。希望最终能够展现出一个好的效果。
+  </p>
 </div>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  margin: 10px 0;
+}
+
+p {
+  text-indent: 2em;
+  line-height: 1.5;
+}
+</style>
