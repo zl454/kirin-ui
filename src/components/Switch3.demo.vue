0550eaf89b5e150da556d9518e3e4f0491dce19f
zl454 <531720074@qq.com>
2020-09-19 18:46:43 +0800

    完成 SwitchDemo 组件展示

---
+++ src/components/Switch3.demo.vue
@@ -0,0 +1,29 @@
+<demo>
+  改变大小
+</demo>
+<template>
+大：
+<Switch v-model:value="bool" size="big" />
+默认：
+<Switch v-model:value="bool" />
+小：
+<Switch v-model:value="bool" size="small" />
+</template>
+
+<script lang="ts">
+import {
+  ref
+} from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const bool = ref(false);
+    return {
+      bool,
+    };
+  },
+};
+</script>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,30 +1,9 @@
 <template>
 <div>
-  <h1>Switch 组件演示</h1>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <Switch1Demo />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')" />
-    </div>
-  </div>
-  <div class="demo">
-    <h2>支持 disabled</h2>
-    <div class="demo-component">
-      <Switch2Demo />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')" />
-    </div>
-  </div>
+  <h1>Switch 组件展示</h1>
+  <Demo :component="Switch1Demo" />
+  <Demo :component="Switch2Demo" />
+  <Demo :component="Switch3Demo" />
 </div>
 <!--
   </div>
@@ -80,24 +59,20 @@
 import {
   ref
 } from "vue";
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
-import "prismjs";
-import "../../node_modules/prismjs/themes/prism.css"; //import问题待解决
-const Prism = (window as any).Prism;
+import Switch3Demo from "./Switch3.demo.vue";
+import Demo from "./Demo.vue";
 export default {
   components: {
-    Button,
+    Demo,
   },
   setup() {
     const bool = ref(false);
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism,
+      Switch3Demo,
     };
   },
 };
