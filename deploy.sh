bf8a51b0146cc90d586b787b29e6c0551cc67e81
zl454 <531720074@qq.com>
2020-09-29 12:20:20 +0800

    一键部署官网

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-rm -rf dist
-yarn build
-cd dist
-git init
-git add .
-git commit -m 'update'
-git branch -M master
-git remote add origin git@github.com:zl454/kylin-ui-website.git
-git push -f -u origin master
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M master &&
+git remote add origin git@github.com:zl454/kylin-ui-website.git &&
+git push -f -u origin master &&
+cd -
+echo https://zl454.github.io/kylin-ui-website/index.html
 
