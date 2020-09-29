83a4bd0aa7ac6021810e390993d470b5d69d23fb
zl454 <531720074@qq.com>
2020-09-29 12:38:17 +0800

    显示隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
     <component :is="component" />
   </div>
   <div class="demo-actions">
-    <Button @click="toggleCode">查看代码</Button>
+    <Button @click="hiddenCode" v-if="codeVisible">隐藏代码</Button>
+    <Button @click="showCode" v-else>查看代码</Button>
   </div>
   <div class="demo-code" v-if="codeVisible">
     <pre class="language-html" v-html="html" />
@@ -38,13 +39,15 @@ export default {
         "html"
       );
     });
-    const toggleCode = () => (codeVisible.value = !codeVisible.value);
+    const showCode = () => (codeVisible.value = true);
+    const hiddenCode = () => (codeVisible.value = false);
     const codeVisible = ref(false);
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode,
+      showCode,
+      hiddenCode,
     };
   },
 };
